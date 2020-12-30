d289197b98ede1b072aa27239a68674c4f7bc18a
Farefure <1537790759@qq.com>
2020-12-30 15:37:38 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <main-tab-bar/>
+  </div>
+</template>
+
+<script>
+  import MainTabBar from 'components/content/mainTabBar/MainTabBar'
+  export default {
+   data() {
+     return {
+     }
+   },
+    name: 'App',
+    components: {
+      MainTabBar
+    },
+  }
+</script>
+
+<style>
+  @import "assets/css/base.css";
+</style>
