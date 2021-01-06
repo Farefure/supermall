d971063f19afae0cd59dbebc324951fbb237f861
Farefure <1537790759@qq.com>
2021-01-06 15:00:19 +0800

    first commit

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    <h2>Home</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
   </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    NavBar
+  }
 }
 </script>
 
 <style>
-
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+  }
 </style>
